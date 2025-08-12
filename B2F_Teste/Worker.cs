540016cbee31cfd4d631c452f1bfa866259cf447
Wilson Junior <wilson.assis.junior@gmail.com>
2025-08-12 17:41:49 -0300

    new commit

--- B2F_Teste/Worker.cs
+++ B2F_Teste/Worker.cs
@@ -9,6 +9,8 @@ namespace B2F_Teste
                 Console.WriteLine("1");
                 Console.WriteLine("2");
                 Console.WriteLine("3");
+                Console.WriteLine("1,2,3 testando");
+                Console.WriteLine("1, 2, 3, 4 testando");
 
                 Console.WriteLine("Serviço iniciado");
                 Console.WriteLine("Serviço finalizado");
