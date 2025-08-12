d55b7a90590a6b4502faa78b4da5501a46205469
Wilson Junior <wilson.assis.junior@gmail.com>
2025-08-12 17:33:22 -0300

    Att

---
+++ B2F_Teste/DataContractJsonSerializerStrategy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
+using Squirrel.Json.Reflection;
+
+namespace B2F_Teste
+{
+    internal class DataContractJsonSerializerStrategy : PocoJsonSerializerStrategy
+    {
+        public DataContractJsonSerializerStrategy()
+        {
+            GetCache = new ReflectionUtils.ThreadSafeDictionary<Type, IDictionary<string, ReflectionUtils.GetDelegate>>(GetterValueFactory);
+            SetCache = new ReflectionUtils.ThreadSafeDictionary<Type, IDictionary<string, KeyValuePair<Type, ReflectionUtils.SetDelegate>>>(SetterValueFactory);
+        }
+
+        internal override IDictionary<string, ReflectionUtils.GetDelegate> GetterValueFactory(Type type)
+        {
+            if (ReflectionUtils.GetAttribute(type, typeof(DataContractAttribute)) == null)
+            {
+                return base.GetterValueFactory(type);
+            }
+
+            IDictionary<string, ReflectionUtils.GetDelegate> dictionary = new Dictionary<string, ReflectionUtils.GetDelegate>();
+            string jsonKey;
+            foreach (PropertyInfo property in ReflectionUtils.GetProperties(type))
+            {
+                if (property.CanRead && !ReflectionUtils.GetGetterMethodInfo(property).IsStatic && CanAdd(property, out jsonKey))
+                {
+                    dictionary[jsonKey] = ReflectionUtils.GetGetMethod(property);
+                }
+            }
+
+            foreach (FieldInfo field in ReflectionUtils.GetFields(type))
+            {
+                if (!field.IsStatic && CanAdd(field, out jsonKey))
+                {
+                    dictionary[jsonKey] = ReflectionUtils.GetGetMethod(field);
+                }
+            }
+
+            return dictionary;
+        }
+
+        internal override IDictionary<string, KeyValuePair<Type, ReflectionUtils.SetDelegate>> SetterValueFactory(Type type)
+        {
+            if (ReflectionUtils.GetAttribute(type, typeof(DataContractAttribute)) == null)
+            {
+                return base.SetterValueFactory(type);
+            }
+
+            IDictionary<string, KeyValuePair<Type, ReflectionUtils.SetDelegate>> dictionary = new Dictionary<string, KeyValuePair<Type, ReflectionUtils.SetDelegate>>();
+            string jsonKey;
+            foreach (PropertyInfo property in ReflectionUtils.GetProperties(type))
+            {
+                if (property.CanWrite && !ReflectionUtils.GetSetterMethodInfo(property).IsStatic && CanAdd(property, out jsonKey))
+                {
+                    dictionary[jsonKey] = new KeyValuePair<Type, ReflectionUtils.SetDelegate>(property.PropertyType, ReflectionUtils.GetSetMethod(property));
+                }
+            }
+
+            foreach (FieldInfo field in ReflectionUtils.GetFields(type))
+            {
+                if (!field.IsInitOnly && !field.IsStatic && CanAdd(field, out jsonKey))
+                {
+                    dictionary[jsonKey] = new KeyValuePair<Type, ReflectionUtils.SetDelegate>(field.FieldType, ReflectionUtils.GetSetMethod(field));
+                }
+            }
+
+            return dictionary;
+        }
+
+        private static bool CanAdd(MemberInfo info, out string jsonKey)
+        {
+            jsonKey = null;
+            if (ReflectionUtils.GetAttribute(info, typeof(IgnoreDataMemberAttribute)) != null)
+            {
+                return false;
+            }
+
+            DataMemberAttribute dataMemberAttribute = (DataMemberAttribute)ReflectionUtils.GetAttribute(info, typeof(DataMemberAttribute));
+            if (dataMemberAttribute == null)
+            {
+                return false;
+            }
+
+            jsonKey = (string.IsNullOrEmpty(dataMemberAttribute.Name) ? info.Name : dataMemberAttribute.Name);
+            return true;
+        }
+    }
+}
--- B2F_Teste/Worker.cs
+++ B2F_Teste/Worker.cs
@@ -1,36 +1,20 @@
-using Squirrel;
-
 namespace B2F_Teste
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
-
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (true)
             {
-                var manager = await UpdateManager.GitHubUpdateManager("https://github.com/b2finance/B2F_Teste");
-                Console.WriteLine("Versão atual: " + manager.CurrentlyInstalledVersion().ToString());
+                Console.WriteLine("1");
+                Console.WriteLine("2");
+                Console.WriteLine("3");
 
-                var updateInfo = await manager.CheckForUpdate();
-
-                if (updateInfo.ReleasesToApply.Count > 0)
-                {
-                    Console.WriteLine("Atualização disponível: " + updateInfo.FutureReleaseEntry.Version.ToString());
-                    await manager.UpdateApp();
-                    Console.WriteLine("Aplicativo atualizado para a versão: " + updateInfo.FutureReleaseEntry.Version.ToString());
-                }
-                else
-                {
-                    Console.WriteLine("Nenhuma atualização disponível.");
-                }
+                Console.WriteLine("Serviço iniciado");
+                Console.WriteLine("Serviço finalizado");
+                Console.ReadLine();
             }
         }
+
     }
 }
